6e202b2dedf94056d759cabb99250aadf667174c
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-21 23:12:15 +0600

    Create P9-Encrement i++.c

---
+++ P9-Encrement i++.c
@@ -0,0 +1,17 @@
+/* Calculate the area of a circle */
+/* Page: 436 */
+/* Program: 8 */
+
+#include<stdio.h>
+
+int main(){
+    
+    int j;
+    int i = 1;
+    j = i ++; // i = 1
+    
+    printf("i=%d\n ",i); // i = 2
+    printf("j=%d",j); // j = 1
+    return 0;
+    
+    }
