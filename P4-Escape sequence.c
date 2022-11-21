ec373d2d09c86a853a8028afe7d6228f04d006ad
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-21 14:42:15 +0600

    Create P4-Escape sequence.c

---
+++ P4-Escape sequence.c
@@ -0,0 +1,12 @@
+// Uses of escape sequence or you can say new line "\n"
+// Page: 430
+// Program: 4
+
+#include<stdio.h>
+
+int main(){
+    
+    printf("Uses of \n escape sequence ");
+    return 0;
+    
+    }
