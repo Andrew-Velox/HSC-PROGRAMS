9a52f62876843904fef11589989e1c4a1b78c8f7
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-21 15:12:23 +0600

    Create P7-Using Variables.c

---
+++ P7-Using Variables.c
@@ -0,0 +1,27 @@
+/* Uses of Veriable */
+/* Page: 432 */
+/* Program: 7 */
+
+
+#include<stdio.h>
+
+int main(){
+
+    const int height = 100;  /*int constant */
+
+    const float number = 3.14; /*Real constant */
+
+    const char letter = 'A';  /*char constant/*/
+
+    const char letter_sequence[10] = "ABCDEFGHI";  /*string constant*/
+
+    printf("value of height : %d\n", height);
+
+    printf("value of number : %f \n", number );
+
+    printf("value of letter : %c \n", letter );
+
+    printf("value of letter_sequence : %s \n", letter_sequence);
+
+    return 0;
+}
