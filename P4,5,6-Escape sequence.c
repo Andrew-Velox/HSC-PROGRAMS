86231111331c0993e1be766990a4d91d9f1f38f1
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-21 14:53:53 +0600

    Update and rename P4-Escape sequence.c to P4,5,6-Escape sequence.c

---
+++ P4,5,6-Escape sequence.c
@@ -0,0 +1,16 @@
+// Using escape sequence \n(new line),\t(tab),\r(return)
+// Page: 430-431
+// Program: 4-6
+
+#include<stdio.h>
+
+int main(){
+    
+    printf("Uses of \n escape sequence ");
+    printf("\n\t Uses of escape sequence ");
+    printf("\n\t\t Uses of escape sequence ");
+    printf("\n\t Uses of escape sequence ");
+    printf("\n\t\r Uses of escape sequence ");
+    return 0;
+    
+    }
