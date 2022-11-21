61d67c8f7d574a05a7c5dd2a1578c85bb8fb7e90
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-21 23:14:25 +0600

    Create  P10-Encrement ++i.c

---
+++ P10-Encrement ++i.c
@@ -0,0 +1,17 @@
+/* USES OF INCREMENT OPERATOR */
+/* Page: 436 */
+/* Program: 10 */
+
+#include<stdio.h>
+
+int main(){
+    
+    int j;
+    int i = 1;
+    j = ++i; // i = 1
+    
+    printf("i=%d\n ",i); // i = 2
+    printf("j=%d",j); // j = 2
+    return 0;
+    
+    }
