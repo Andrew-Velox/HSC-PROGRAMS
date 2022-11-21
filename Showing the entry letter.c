9712bda8553670ddf71f8a1945f57c9d1d2207cf
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-21 13:00:21 +0600

    Create Showing the entry letter.c

---
+++ Showing the entry letter.c
@@ -0,0 +1,13 @@
+/* Showing the entry letter */
+/* Page: 426 */
+
+#include <stdio.h>
+int main(){
+        
+    char ch;
+    printf(" Enter The letter ");
+    ch = getchar();
+    printf("\n You entered the letter: %c",ch);
+    return 0;
+    
+    }
