5c68f4e31715a1db071b6302b54201673e686aee
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-22 10:31:09 +0600

    Create P12-Assignment Statenent.c

---
+++ P12-Assignment Statenent.c
@@ -0,0 +1,16 @@
+/* Statement */
+/* Page: 442 */
+/* Program: 12 */
+
+#include <stdio.h>
+
+int main(){
+    
+    int num1=24, num2 = 48, sum;
+    sum = num1 + num2;
+    printf("Summation of t2o number: %d",sum);
+    return 0;
+    
+    
+    
+    }
