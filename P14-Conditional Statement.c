53b75802a254508cc843b020970efdc93a232cfa
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-22 11:26:35 +0600

    Create P14-Conditional Statement.c

---
+++ P14-Conditional Statement.c
@@ -0,0 +1,18 @@
+/* Conditional statement (if) */
+/* Page: 445 */
+/* Program: 14 */
+
+#include <stdio.h>
+
+int main(){
+    
+    int number;
+    printf(" Enter Your Number: ");
+    scanf("%d",&number);
+    
+    if(number>=33)
+    printf("\n You pass in the exam.");
+    return 0;
+    
+    
+    }
