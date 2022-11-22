b35e8c0f4b33d75c69794a94dec9f49d06baae20
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-22 22:57:43 +0600

    Create P18- Calculate even or odd number.c

---
+++ P18- Calculate even or odd number.c
@@ -0,0 +1,21 @@
+/* Conditional statement (if & else) */
+/* Page: 447 */
+/* Program: 18 */
+
+#include <stdio.h>
+
+int main(){
+    
+    int n;
+    
+    printf(" Enter a number: ");
+    scanf("%d",&n);
+    
+    if (n%2==0)
+    printf("\n This is even number.");
+    else
+    printf("\n This is odd number.");
+    
+    return 0;
+    
+    }
