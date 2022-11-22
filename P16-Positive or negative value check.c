e28801a8ac25f9855aa6194abc6ac9fc3123be7f
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-22 22:37:53 +0600

    Create P16-Positive or negative value check.c

---
+++ P16-Positive or negative value check.c
@@ -0,0 +1,19 @@
+/* Conditional statement (if) */
+/* Page: 445 */
+/* Program: 16 */
+
+#include <stdio.h>
+
+int main(){
+    
+    int num;
+    
+    printf(" Enter the number: ");
+    scanf("%d",&num);
+    
+    if (num>0)
+    printf("\n The number is positive.");
+    if (num<0)
+    printf("\n The number is negative.");
+    return 0;
+    }
