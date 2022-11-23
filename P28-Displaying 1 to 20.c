07812cbc560f4ce5ba585da0a9b9b6d778d2c9d1
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-23 18:39:07 +0600

    Create P28-Displaying 1 to 20.c

---
+++ P28-Displaying 1 to 20.c
@@ -0,0 +1,23 @@
+/* Displaying 1 to 20 */
+/* Page: 452 */
+/* Program: 28 */
+
+
+#include <stdio.h>
+
+int main(){
+  
+  int i = 1;
+  
+  do{
+      
+      printf("%d ",i);
+      i=i+1;
+      
+      }while (i < 21);
+      return 0;
+  
+  
+  
+  
+    }
