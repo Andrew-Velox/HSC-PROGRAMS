8b4a7c98cb248702a509f5a7b561688db613bf3f
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-23 18:47:07 +0600

    Create P29-Displaying A-Z Capital letter.c

---
+++ P29-Displaying A-Z Capital letter.c
@@ -0,0 +1,26 @@
+/* Displaying A-Z  */
+/* Page: 452 */
+/* Program: 29 */
+
+
+#include <stdio.h>
+
+int main(){
+  
+  char ch = 65;
+  do{
+
+    printf("%c ",ch);
+    
+    
+    ch = ch + 1;
+    
+    
+    }while((ch>='A')&&(ch<='Z'));
+    return 0;
+    
+    
+    
+    
+    
+     }
