5640ca00c66e4e6ef22a7193777901962181b314
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-23 16:38:31 +0600

    Create P27-Displaying A-Z Alphabet.c

---
+++ P27-Displaying A-Z Alphabet.c
@@ -0,0 +1,21 @@
+/* Displaying A-Z Alphabet */
+/* Page: 451 */
+/* Program: 27 */
+
+
+#include <stdio.h>
+
+int main(){
+  
+  
+  char alphabet='A';
+  
+  do{
+      printf("% c\t ", alphabet ++);
+  
+      }
+      while(alphabet<='Z');
+      return 0;
+  
+  
+    }
