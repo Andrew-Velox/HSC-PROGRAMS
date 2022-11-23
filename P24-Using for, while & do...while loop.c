85b568ec2fdd7e5f0b398c8da08ad12a3786bb1e
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-23 15:00:44 +0600

    Create P24-Using for, while & do...while loop.c

---
+++ P24-Using for, while & do...while loop.c
@@ -0,0 +1,66 @@
+/* Looping (for, while & do...while) */
+/* Page: 449 */
+/* Program: 24 */
+
+
+#include <stdio.h>
+
+
+/*void main(){
+    
+    
+    
+    int i;
+    
+    for (i=1; i <=7; i ++){
+        
+        printf("\n Hello ");
+        printf("%d",i);
+        
+        }
+    
+    
+   
+    
+    //return 0;
+    
+    
+    }*/
+
+
+
+
+/*int main(){
+     int i = 1;
+     while (i<=7)
+     {
+         
+         printf("\n\t Hello World.");
+         
+         i = i +1;
+         
+         }    
+    
+    
+    }*/
+    
+    
+void main(){
+    
+    int i=1;
+    
+    do{
+        
+        printf("\n Andrew-");
+        printf("%d",i);
+        
+        i =i +1;
+        
+        
+        }while(i<=7);
+    
+    
+    
+    
+    
+    }
