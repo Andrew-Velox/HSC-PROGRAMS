219256a3d809946c9e9e7664f5a1b10e2794c3cf
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-23 22:39:09 +0600

    Create P31-Displaying 1-5.c

---
+++ P31-Displaying 1-5.c
@@ -0,0 +1,41 @@
+/* Displaying 1-5 using for,while &do..while loop */
+/* Page: 453 */
+/* Program: 31 */
+
+
+#include <stdio.h>
+
+int main(){
+  
+  /*     // using for loop//
+  int i;
+  
+  for (i=1;i<=5;i++)
+ 
+  printf("%d \n",i);
+  
+  */
+  
+  
+  
+  
+  /*     // using while loop //
+  int i =1;
+  
+  while(i<=5){
+      printf("%d \n",i);
+      i = i +1;
+      }
+      */
+      
+      
+          // using do...while loop //
+  int i = 1;
+  
+  do{
+      
+      printf("%d \n",i);
+      i = i + 1;
+      
+      }while(i<=5);
+ }
