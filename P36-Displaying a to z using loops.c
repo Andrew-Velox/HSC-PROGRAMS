f903b4dd88a98ab09cd8e7d4a31b5d299898a132
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-24 20:59:55 +0600

    Create P36-Displaying a to z using loops.c

---
+++ P36-Displaying a to z using loops.c
@@ -0,0 +1,64 @@
+/* Displaying a to z using for,while &do..while loop */
+/* Page: 455 */
+/* Program: 36 */
+
+
+#include <stdio.h>
+
+int main(){
+  
+  /*  // Using for Loop //
+  
+  char x;
+  
+  
+  
+  for(x='a'; x<='z';x++){
+      
+      
+      
+  
+      printf("%c ",x);
+     
+      } 
+      
+      
+      */
+  
+  
+  
+ /*   // using while loop //
+  
+  char abc;
+  abc = 'a';
+  while (abc<='z'){
+      
+      printf("%c ",abc);
+      
+      abc = abc+1;
+      
+      } 
+  
+  */
+  
+//  /* // using do...while loop//
+   
+  int abc;
+  abc = 'a';
+  
+  
+  
+  do{
+      
+      
+      printf("%c ",abc);
+      abc = abc+1;
+      
+      
+      }while(abc<='z'); 
+  
+  return 0;
+  
+//  */
+  
+ }
