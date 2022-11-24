6b9c83017c04eba063ad2f16009994927518af95
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-25 03:03:02 +0600

    Update P42-Multiplication of 1×2×3×....n series.c

--- P42-Multiplication of 1×2×3×....n series.c
+++ P42-Multiplication of 1×2×3×....n series.c
@@ -1,3 +1,53 @@
-Displaying the multiplication of series 1×2×3×....n  using for,while &do..while loop */
+/* Displaying the multiplicatio of series 1×2×3×....n  using for,while &do..while loop */
 /* Page: 458 */
-/* Program: 41 */
+/* Program: 42 */
+
+
+#include <stdio.h>
+
+int main(){
+    /*  //Using for loop //
+    
+    int x,num,sum=1;
+    
+    printf(" Enter last number of the series: ");
+    scanf("%d",&num);
+    
+    for (x=1;x<=num;x=x+1){
+        
+        
+        sum = sum*x;
+        
+        }
+    printf(" %d ",sum);
+    */
+    
+    /* // Using while loop//
+    int x=1,num,sum=1;
+    printf(" Enter last number of the series: ");
+    scanf("%d",&num);
+    
+    while(x<=num){
+        //x= x+1;
+        sum = sum*x;
+        x=x+1;
+        }
+    printf("%d",sum);
+    
+    */
+   //*  //using do....While loop//
+    
+    int x=1,num,sum=1;
+    printf(" Enter last number of the series: ");
+    scanf("%d",&num);
+    
+    do{
+        //x= x+1;
+        sum = sum*x;
+        x=x+1;
+        }while(x<=num);
+    printf("%d",sum);
+    //*/
+    return 0;
+    
+    }
