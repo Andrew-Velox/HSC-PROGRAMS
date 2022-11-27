70e79ef43062a74e27ee8da1d0ccbaced2ddddc4
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-28 00:33:06 +0600

    Create P75-Area of a circle.c

---
+++ P75-Area of a circle.c
@@ -0,0 +1,22 @@
+/* Area of a circle */
+/* Page: 475 */
+/* Program: 75 */
+
+
+#include <stdio.h>
+int main(){
+    
+    float r,pi,area;
+    
+    pi = 3.14;
+    
+    printf(" Enter the value of r = ");
+    scanf("%f",&r);
+    
+    area = pi*r*r;
+    
+    printf("\n Area = %.2f",area);
+    return 0;
+    
+    
+    }
