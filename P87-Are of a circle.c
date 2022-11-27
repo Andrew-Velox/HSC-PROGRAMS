1779edc38a13368ba9f1f39974cf3470f80d06c1
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-27 23:55:36 +0600

    Create P87-Are of a circle.c

---
+++ P87-Are of a circle.c
@@ -0,0 +1,21 @@
+/* Area of a circle */
+/* Page: 476 */
+/* Program: 87 */
+
+
+#include <stdio.h>
+int main(){
+    
+    float b,h,area;
+    
+    
+    printf(" Enter the value of b & h = ");
+    scanf("%f %f",&b,&h);
+    
+    area = (b*h)/2;
+    
+    printf("\n Area = %.2f",area);
+    return 0;
+    
+    
+    }
