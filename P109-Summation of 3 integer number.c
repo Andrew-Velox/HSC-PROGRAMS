d52d489233ffeb4a7f0d86498ed408e959ee5f14
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-28 16:54:34 +0600

    Create P109-Summation of 3 integer number.c

---
+++ P109-Summation of 3 integer number.c
@@ -0,0 +1,20 @@
+/* Summation of 3 integer number */
+/* Page: 476 */
+/* Program: 109 */
+
+
+#include <stdio.h>
+int main(){
+    
+    int a,b,c,sum;
+    
+    printf(" Enter  the value of a,b & c = ");
+    scanf("%d %d %d",&a,&b,&c);
+    
+    sum = a+b+c;
+    
+    printf("\n Sum = %d",sum);
+    return 0;
+    
+    
+    }
