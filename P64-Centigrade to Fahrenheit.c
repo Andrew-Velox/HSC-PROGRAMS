1aa1ff9c7a150dca5ed5a24a2f316112b6d8eab0
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-11-28 16:46:33 +0600

    Create P64-Centigrade to Fahrenheit.c

---
+++ P64-Centigrade to Fahrenheit.c
@@ -0,0 +1,20 @@
+/* Centigrade to Fahrenheit */
+/* Page: 473 */
+/* Program: 64 */
+
+
+#include <stdio.h>
+int main(){
+    
+    float c,F;
+    
+    printf(" Enter  = ");
+    scanf("%f",&c);
+    
+    F = 9*c/5+32;
+    
+    printf("\n Area = %.2f",F);
+    return 0;
+    
+    
+    }
