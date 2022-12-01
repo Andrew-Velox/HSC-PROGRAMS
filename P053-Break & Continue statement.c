93d43bde5f9383dbf85a507c96de56a17ba764e1
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-01 20:27:34 +0600

    Create P053-Break & Continue statement.c

---
+++ P053-Break & Continue statement.c
@@ -0,0 +1,23 @@
+/* USES OF break & continue statement  */
+/* Page: 464 */
+/* Program: 53 */
+
+
+#include <stdio.h>
+
+int main(){
+    
+    int i;
+    
+    for (i=1; i<=10; i = i+1){
+        
+        printf("\n Computer programming.");
+        
+        if(i==6){
+            
+            break;
+            }
+ 
+        }
+    
+    }
