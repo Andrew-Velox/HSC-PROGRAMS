a6da04273addc49dc7e3c35e440ceb81840f46db
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-01 20:49:46 +0600

    Create P054-Break statement.c

---
+++ P054-Break statement.c
@@ -0,0 +1,25 @@
+/* USES OF break & continue statement  */
+/* Page: 464 */
+/* Program: 54 */
+
+
+#include <stdio.h>
+
+int main(){
+    
+    int i = 10;
+    
+    while (i < 20){
+        
+        printf("\n %d",i);
+        
+        i = i +1;
+        
+        if(i>15){
+            
+            break;
+            }
+ 
+        }
+    return 0;
+    }
