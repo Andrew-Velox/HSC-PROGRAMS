0c91e3e15ed7df9f311d12b1f8cf6ffe1a288df8
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-02 23:27:09 +0600

    Create P057-Uses of Continue statement.c

---
+++ P057-Uses of Continue statement.c
@@ -0,0 +1,23 @@
+/*  Uses of Continue statement  */
+/* Page: 465 */
+/* Program: 57 */
+
+#include <stdio.h>
+
+int main(){
+    
+    int n;
+    
+    for (n=1;n<=5;n=n+1){
+        
+        if (n == 3)
+        
+        continue;
+        printf("\n HSX Exam. %d",n);
+    
+        }
+    
+    
+    return 0;
+    
+    }
