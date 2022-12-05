3354315771c0656aaea2b9499ad74d7445235ee8
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-05 20:25:13 +0600

    Create P066-Kelvin to Centigrade.c

---
+++ P066-Kelvin to Centigrade.c
@@ -0,0 +1,21 @@
+/*  Kelvin to Centigrade/Celsius */
+/* Page: 474 */
+/* Program: 66 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float c,k;
+     
+     printf(" Enter the Value of c : ");
+     scanf("%f",&k);
+     
+     
+     c = 5*(k-273)/5;
+    
+     
+     printf("%.2f",c);
+     return 0;
+     
+    }
