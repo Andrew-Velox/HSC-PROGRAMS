ed582129fa443e0126661d7c8203cf42776dd84d
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-05 21:10:49 +0600

    Create P068-Centimeter to Inch.c

---
+++ P068-Centimeter to Inch.c
@@ -0,0 +1,21 @@
+/* Centimeter to  inch */
+/* Page: 474 */
+/* Program: 68 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float cm,in;
+     
+     printf(" Enter the Value of f : ");
+     scanf("%f",&cm);
+     
+     
+     in = cm/2.54;
+    
+     
+     printf("%.2f",in);
+     return 0;
+     
+    }
