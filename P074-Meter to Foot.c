416f23622376890cac999786026f14ff64e018dd
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-05 22:07:44 +0600

    Create P074-Meter to Foot.c

---
+++ P074-Meter to Foot.c
@@ -0,0 +1,19 @@
+/* Meter to Foot */
+/* Page: 474 */
+/* Program: 72 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float m,f;
+     
+     printf(" Enter the Value of m : ");
+     scanf("%f",&m);
+     
+     
+     f = m * 3.28;
+    
+     
+     printf("%.2f",f);
+     return 0;
