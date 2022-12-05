d5a7f89f47eda86df15f8683177d21e1ff68dc76
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-05 21:06:49 +0600

    Create P069-Fahrenheit to Centigrade.c

---
+++ P069-Fahrenheit to Centigrade.c
@@ -0,0 +1,21 @@
+/*  Fahrenheit to Centigrade  */
+/* Page: 474 */
+/* Program: 69 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float f,c;
+     
+     printf(" Enter the Value of f : ");
+     scanf("%f",&f);
+     
+     
+     c = 5*(f-32)/9;
+    
+     
+     printf("%.2f",c);
+     return 0;
+     
+    }
