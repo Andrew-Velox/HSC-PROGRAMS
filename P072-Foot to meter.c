287b2f1b848ea062f27d2374ae8ff667276c3ca1
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-05 22:02:08 +0600

    Create P072-Foot to meter.c

---
+++ P072-Foot to meter.c
@@ -0,0 +1,21 @@
+/* Foot to meter */
+/* Page: 474 */
+/* Program: 72 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float cm,M;
+     
+     printf(" Enter the Value of f : ");
+     scanf("%f",&f);
+     
+     
+     M = f/3.28;
+    
+     
+     printf("%.2f",M);
+     return 0;
+     
+    }
