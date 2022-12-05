7d2ea0b4d7c0c823b687926aefa31adf9b660652
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-05 20:17:23 +0600

    Create P065-Celsius or Centigrade to Kelvin.c

---
+++ P065-Celsius or Centigrade to Kelvin.c
@@ -0,0 +1,21 @@
+/*  Centigrade/Celsius to Kelvin */
+/* Page: 474 */
+/* Program: 66 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float c,k;
+     
+     printf(" Enter the Value of c : ");
+     scanf("%f",&c);
+     
+     
+     k = c + 273.15;
+    
+     
+     printf("%.2f",k);
+     return 0;
+     
+    }
