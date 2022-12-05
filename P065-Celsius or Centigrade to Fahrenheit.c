7388ea03584bf85640c0da2118c57f972b5d910b
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-05 20:10:40 +0600

    Create P065-Celsius or Centigrade to Fahrenheit.c

---
+++ P065-Celsius or Centigrade to Fahrenheit.c
@@ -0,0 +1,21 @@
+/*  Centigrade/Celsius to Fahrenheit */
+/* Page: 474 */
+/* Program: 65 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float n,f;
+     
+     printf(" Enter the Value of c : ");
+     scanf("%f",&n);
+     
+     
+     f = (n*9)/5+32;
+     
+     
+     printf("%.2f",f);
+     return 0;
+     
+    }
