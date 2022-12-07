9ed7f3d4312bf09c274547bb8038d8a9d2889d79
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 01:13:09 +0600

    Create P080-Area of ​​square.c

---
+++ P080-Area of ​​square.c
@@ -0,0 +1,23 @@
+/* Area of ​​square */
+/* Page: 478 */
+/* Program: 80 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float s,a;
+     
+     
+     printf(" Side  = ");
+     scanf("%f",&a);
+     
+     
+     
+     s = a*a;
+     
+     
+     printf("Area = %.2f",s);
+     return 0;
+     
+    }
