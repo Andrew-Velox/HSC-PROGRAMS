07f4efab81889751dee7091bb772ad71020535aa
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-07 18:28:11 +0600

    Create P077-Circumference of the circle.py

---
+++ P077-Circumference of the circle.py
@@ -0,0 +1,22 @@
+/* Circumference of the circle */
+/* Page: 478 */
+/* Program: 77 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float pi,s,r;
+     
+     pi = 3.14;
+     
+     printf(" Enter the value of r = ");
+     scanf("%f",&r);
+     
+     s = 2 * pi * r;            
+     
+     
+     printf("%.2f",s);
+     return 0;
+     
+    }
