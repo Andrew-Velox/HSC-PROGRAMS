801efc3b086e46c2221626705fe8aa5ad32fa34c
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 02:16:56 +0600

    Create P089-Perimeter of a triangle.c

---
+++ P089-Perimeter of a triangle.c
@@ -0,0 +1,19 @@
+/* Perimeter of a triangle  */
+/* Page: 478 */
+/* Program: 89 */
+
+#include <stdio.h>
+
+int main(){
+     float s,a,b,c;
+     
+     printf(" Enter the value of a,b & c = ");
+     scanf("%f %f %f",&a,&b,&c);
+     
+     s = a + b + c;
+     
+     
+         
+     printf(" Answer = %.2f",s);
+     return 0;
+    }
