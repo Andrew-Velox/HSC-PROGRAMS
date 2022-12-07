27c4d10e0de24b677729c4786d88d6168fb19e88
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 02:10:13 +0600

    Create P083-Area of Rhombus.c

---
+++ P083-Area of Rhombus.c
@@ -0,0 +1,19 @@
+/* Area of Rhombus  */
+/* Page: 478 */
+/* Program: 83 */
+
+#include <stdio.h>
+
+int main(){
+     float s,a,b;
+     
+     printf(" Enter the value of a & b= ");
+     scanf("%f %f",&a,&b);
+     
+     s = 0.5 * (a*b);
+     
+     
+         
+     printf(" Answer = %.2f",s);
+     return 0;
+    }
