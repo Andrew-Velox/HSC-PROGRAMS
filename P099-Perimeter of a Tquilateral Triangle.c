18a197bf8bf292832c13aa4c9f98c8bd854f8fdc
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 03:50:00 +0600

    Create P099-Perimeter of a Tquilateral Triangle.c

---
+++ P099-Perimeter of a Tquilateral Triangle.c
@@ -0,0 +1,22 @@
+/*  Perimeter of a Tquilateral Triangle */
+/* Page: 478 */
+/* Program: 99 */
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+     float s,a,b,c;
+     
+     printf( "\t\t\tnote: a = length; b = width; c = height");
+     printf("\n\n Enter the value of a= ");
+     scanf("%f",&a);
+     
+     s = 3*a;
+     
+     
+         
+     printf(" Answer = %.2f",s);
+     return 0;
+    }
