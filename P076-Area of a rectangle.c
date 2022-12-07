f91414d9074d86b239c8153e9936cabf9d081c36
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-07 19:21:52 +0600

    Create P076-Area of a rectangle.c

---
+++ P076-Area of a rectangle.c
@@ -0,0 +1,24 @@
+/* Area of ​​a rectangle  */
+/* Page: 478 */
+/* Program: 76 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float s,a,b;
+     
+     
+     printf(" Length  = ");
+     scanf("%f",&a);
+     printf(" Width = ");
+     scanf("%f",&b);
+     
+     
+     s = a * b ;
+     
+     
+     printf("%.2f",s);
+     return 0;
+     
+    }
