e49ec6abfeb680d77f51265f51bba21d48cc0b29
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 01:00:05 +0600

    Create P079-Area of the parallelogram.c

---
+++ P079-Area of the parallelogram.c
@@ -0,0 +1,24 @@
+/* Area of ​​the parallelogram  */
+/* Page: 478 */
+/* Program: 79 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float s,a,b;
+     
+     
+     printf(" Base  = ");
+     scanf("%f",&a);
+     printf(" Height = ");
+     scanf("%f",&b);
+     
+     
+     s = a * b;
+     
+     
+     printf("%.2f",s);
+     return 0;
+     
+    }
