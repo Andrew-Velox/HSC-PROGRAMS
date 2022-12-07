b8d376a994db0f24fca0ccd09afa712c3c923b6b
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 02:06:45 +0600

    Create P085-Perimeter of Rhombus.c

---
+++ P085-Perimeter of Rhombus.c
@@ -0,0 +1,19 @@
+/* Perimeter of Rhombus  */
+/* Page: 478 */
+/* Program: 85 */
+
+#include <stdio.h>
+
+int main(){
+     float s,a;
+     
+     printf(" Enter the value of a= ");
+     scanf("%f",&a);
+     
+     s = 4*a;
+     
+     
+         
+     printf(" Answer = %.2f",s);
+     return 0;
+    }
