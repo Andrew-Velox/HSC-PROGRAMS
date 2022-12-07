543449ca82422b90af8cfc0141c50e96bdd9b896
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 00:56:13 +0600

    Create P078-Range of rectangles.c

---
+++ P078-Range of rectangles.c
@@ -0,0 +1,24 @@
+/* Range of rectangles  */
+/* Page: 478 */
+/* Program: 78 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float s,a,b;
+     
+     
+     printf(" Length  = ");
+     scanf("%f",&a);
+     printf(" Width = ");
+     scanf("%f",&b);
+     
+     
+     s = 2* (a + b) ;
+     
+     
+     printf("%.2f",s);
+     return 0;
+     
+    }
