c931db34ec2637cabbbf4c4b6d83dd5f98b21645
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 01:05:00 +0600

    Create P081-Perimeter of a rectangle.c

---
+++ P081-Perimeter of a rectangle.c
@@ -0,0 +1,24 @@
+/* Perimeter of a rectangle */
+/* Page: 478 */
+/* Program: 81 */
+
+#include <stdio.h>
+
+int main(){
+     
+     float s,a,b;
+     
+     
+     printf(" Length  = ");
+     scanf("%f",&a);
+     printf(" Width = ");
+     scanf("%f",&b);
+     
+     
+     s = 2*(a + b);
+     
+     
+     printf("%.2f",s);
+     return 0;
+     
+    }
