c9b6858cfc7b8bbc1c8bdee4f9624e76f41f9bb0
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 02:26:11 +0600

    Create P086-Perimeter of a Trapezium.c

---
+++ P086-Perimeter of a Trapezium.c
@@ -0,0 +1,19 @@
+/* Perimeter of a Trapezium  */
+/* Page: 478 */
+/* Program: 86 */
+
+#include <stdio.h>
+
+int main(){
+     float s,a,b,c,d;
+     
+     printf(" Enter the value of a,b,c & d= ");
+     scanf("%f %f %f %f",&a,&b,&c,&d);
+     
+     s = a + b + c +d;
+     
+     
+         
+     printf(" Answer = %.2f",s);
+     return 0;
+    }
