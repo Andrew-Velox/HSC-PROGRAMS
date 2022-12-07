b76fbd7a1859c8a96ed9000072a22265dd43e14e
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 03:59:09 +0600

    Create P096-Diagonal of a Cube.c

---
+++ P096-Diagonal of a Cube.c
@@ -0,0 +1,22 @@
+/* Diagonal of a Cube */
+/* Page: 478 */
+/* Program: 96 */
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+     float s,a,b,c;
+     
+     printf( "\t\t\tnote: a = length; b = width; c = height");
+     printf("\n\n Enter the value of a= ");
+     scanf("%f",&a);
+     
+     s = sqrt(3)*a;
+     
+     
+         
+     printf(" Answer = %.2f",s);
+     return 0;
+    }
