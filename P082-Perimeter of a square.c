494777e6c49dde79b2e564d62e0c3b893a3a8a56
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 01:41:23 +0600

    Create P082-Perimeter of a square.c

---
+++ P082-Perimeter of a square.c
@@ -0,0 +1,19 @@
+/* Area of a square  */
+/* Page: 478 */
+/* Program: 82 */
+
+#include <stdio.h>
+
+int main(){
+     float s,a;
+     
+     printf(" Side = ");
+     scanf("%f",&a);
+     
+     s = 4 * a;
+     
+     
+         
+     printf(" Answer = %.2f",s);
+     return 0;
+    }
