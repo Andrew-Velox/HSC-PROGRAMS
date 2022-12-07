2cecf6d9cca528aaabfb90c9d5da3b10c8b251b9
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 03:40:49 +0600

    Create P100-Area of a cube.c

---
+++ P100-Area of a cube.c
@@ -0,0 +1,22 @@
+/* Area of a cube */
+/* Page: 478 */
+/* Program: 100 */
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+     float s,a,b,c;
+     
+     printf( "\t\t\tnote: a = length; b = width; c = height");
+     printf("\n\n Enter the value of a= ");
+     scanf("%f",&a);
+     
+     s = 6*a*a;
+     
+     
+         
+     printf(" Answer = %.2f",s);
+     return 0;
+    }
