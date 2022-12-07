efc5df134576edd82239c8c677689cca593f0e45
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 03:38:45 +0600

    Create P098-Volume of cube a formula.c

---
+++ P098-Volume of cube a formula.c
@@ -0,0 +1,22 @@
+/* Volume of cube a formula */
+/* Page: 478 */
+/* Program: 98 */
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+     float s,a,b,c;
+     
+     printf( "\t\t\tnote: a = length; b = width; c = height");
+     printf("\n\n Enter the value of a= ");
+     scanf("%f",&a);
+     
+     s = a*a*a;
+     
+     
+         
+     printf(" Answer = %.2f",s);
+     return 0;
+    }
