898ba7f33e07a82b65038306a745b2788f7229c3
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 20:36:01 +0600

    Create P061-Uses of function.c

---
+++ P061-Uses of function.c
@@ -0,0 +1,14 @@
+/* Uses of function  */
+/* Page: 470 */
+/* Program: 61 */
+
+#include <stdio.h>
+
+
+int hello(){
+     
+     printf( " Welcome \n ");
+     printf(" C  Programming\n ");
+     printf(" World \n\n\n");
+     
+    }
