7c36d691d8fc72bb2b203dbe138c6ac6fafef44e
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 19:17:14 +0600

    Create P107-Square root of a num.c

---
+++ P107-Square root of a num.c
@@ -0,0 +1,23 @@
+/* Square root of an integer number  */
+/* Page: 478 */
+/* Program: 107 */
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+     float s,r,a,b,c,pi;
+     
+     printf( "\tnote: a = length; b = width; c = height; r = radius ");
+     printf("\n\n Enter the value of a = ");
+     scanf("%f",&a);
+     
+     
+     s = sqrt(a);
+     
+     
+         
+     printf(" Answer = %.2f",s);
+     return 0;
+    }
