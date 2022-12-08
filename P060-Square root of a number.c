0a5c34a1dd6e23843e3019e5943821fb950ccaa6
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 23:24:58 +0600

    Create P060-Square root of a number.c

---
+++ P060-Square root of a number.c
@@ -0,0 +1,23 @@
+/* Square root of a number  */
+/* Page: 469 */
+/* Program: 60 */
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+     float n,r;
+     
+     printf( " Enter a number = ");
+     scanf("%f",&n);
+     
+     
+     
+     
+     r = sqrt(n);
+     
+     printf("\n Answer = %.2f ",r);
+     return 0;
+     
+    }
