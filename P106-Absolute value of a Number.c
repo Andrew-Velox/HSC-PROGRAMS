ebc0c5b49568c054f77d86a779ad284717ee3d88
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 20:21:19 +0600

    Create P106-Absolute value of a Number.c

---
+++ P106-Absolute value of a Number.c
@@ -0,0 +1,24 @@
+/* Absolute value of a Number */
+/* Page: 478 */
+/* Program: 106 */
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+     int s,a;
+     
+     
+     //printf( "\tnote: a = number; b = power; c = height; r = radius ");
+     printf("\n\n Enter the value of a = ");
+     scanf("%d",&a);
+     
+     
+     s = abs(a);
+     
+     
+         
+     printf(" Answer = %d",s);
+     return 0;
+    }
