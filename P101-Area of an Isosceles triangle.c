e2f2830a8ba6242c20b283d0ee1397fc7f1f908e
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-08 18:32:03 +0600

    Create P101-Area of an Isosceles triangle.c

---
+++ P101-Area of an Isosceles triangle.c
@@ -0,0 +1,22 @@
+/* Area of an Isosceles triangle */
+/* Page: 478 */
+/* Program: 101 */
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+     float s,a,b;
+     
+     printf( "\t\t\tnote: a = length; b = width; c = height");
+     printf("\n\n Enter the value of a & b = ");
+     scanf("%f %f",&a,&b);
+     
+     s = (b/4)*sqrt(4*(a*a)-(b*b));
+     
+     
+         
+     printf(" Answer = %.2f",s);
+     return 0;
+    }
