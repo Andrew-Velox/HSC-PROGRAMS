eeb7cc5031a3777d632ed6673573a95597b5287f
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-10 00:37:51 +0600

    Create P05-Escape sequence(\t).c

---
+++ P05-Escape sequence(\t).c
@@ -0,0 +1,15 @@
+// Using escape sequence \t(tab)
+// Page: 431
+// Program: 5
+
+#include<stdio.h>
+
+int main(){
+    
+    printf("Uses of \n escape sequence ");
+    printf("\n\t Uses of escape sequence ");
+    printf("\n\t\t Uses of escape sequence ");
+    printf("\n\t Uses of escape sequence ");
+    return 0;
+    
+    }
