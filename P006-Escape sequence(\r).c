7adbf046c4a1c7ceba3b9d1a26c68ab8850a91b5
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-10 00:42:56 +0600

    Create P006-Escape sequence(\r).c

---
+++ P006-Escape sequence(\r).c
@@ -0,0 +1,14 @@
+// Using escape sequence \r(return)
+// Page: 431
+// Program: 6
+
+#include<stdio.h>
+
+int main(){
+    
+    printf("Uses of escape sequence ");
+    printf("\n\t Uses of escape sequence ");
+    printf("\n\t\r Uses of escape sequence ");
+    return 0;
+    
+    }
