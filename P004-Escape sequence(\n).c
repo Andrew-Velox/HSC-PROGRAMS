2b2b6507bda8b0f56b80f5f82b8794d5b47db03f
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-10 00:44:47 +0600

    Update and rename P004,005,006-Escape sequence.c to P004-Escape sequence(\n).c

---
+++ P004-Escape sequence(\n).c
@@ -0,0 +1,13 @@
+// Using escape sequence \n(new line)
+// Page: 430
+// Program: 4
+
+#include<stdio.h>
+
+int main(){
+    
+    printf("Uses of \n escape sequence ");
+
+    return 0;
+    
+    }
