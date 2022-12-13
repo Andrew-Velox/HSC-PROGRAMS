7998f5042de9c23d70a1de0c12cc3b888bd8233d
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-13 20:19:39 +0600

    Create P138-Even or Odd number check.c

---
+++ P138-Even or Odd number check.c
@@ -0,0 +1,24 @@
+/* Check whether a number is Even or Odd */
+/* Page: 484 */
+/* Program: 138 */
+
+
+#include <stdio.h>
+
+int main(){
+    
+    int n;
+    
+    printf(" Enter a number: ");
+    scanf("%d",&n);
+            
+    if(n%2 == 0)
+    printf(" Even Number");
+    
+    else
+    printf(" Odd Number");
+            
+    
+    return 0;
+    
+    }
