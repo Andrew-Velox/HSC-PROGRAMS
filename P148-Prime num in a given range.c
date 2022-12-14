6ceed2f5ccd7aa63635b9dc1d848d374c720f885
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2022-12-14 23:52:45 +0600

    Create P148-Prime num in a given range.c

---
+++ P148-Prime num in a given range.c
@@ -0,0 +1,29 @@
+/* Displaying a series of prime numbers */
+/* Page: 490 */
+/* Program: 148 */
+
+#include <stdio.h>
+int main(){
+    
+    int i,sum,n;
+    int prime;
+    printf(" Enter the heighest range : ");
+    scanf("%d",&n);
+    printf(" Series of prime number upto %d is  = ",n);
+    
+    for(i = 2; i<=n; i = i+1){
+        
+        prime = 1;             // any number
+        
+        for(sum=2; sum<i; sum = sum +1)
+        
+        if(i%sum == 0){
+            prime = 2;
+            break;
+        }
+        if(prime == 1)
+        printf("%d ",i);
+        
+        }
+
+}
